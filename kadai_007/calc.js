4d1dc924624cbd15b5c115cf29e1035d1e78e6d8
anma2625 <harumun0720@yahoo.co.jp>
2025-05-17 17:35:54 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,29 @@
+//変数numを定義します
+let num = 9
+
+//変数numが３の倍数かどうかを調べます
+if(num% 3 === 0){
+  console.log("3の倍数です");
+}
+else{
+  console.log("3の倍数ではありません");
+}
+
+//変数numが5の倍数かどうかを調べます
+if(num% 5 === 0){
+  console.log("5の倍数です");
+}
+else{
+  console.log("5の倍数ではありません");
+}
+
+//変数3と５の倍数かどうかを調べます
+if(num % 3  === 0 && num % 5 === 0){
+  console.log("3と5の倍数です");
+}
+
+//それ以外の場合
+else{
+  console.log(num); 
+}
+
